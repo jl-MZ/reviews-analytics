a97b6ff1cbe654dd854a9b895f630151d234b010
Jlin <jianglin661@gmail.com>
2020-06-04 21:23:41 +0900

    second review commit

--- review.py
+++ review.py
@@ -14,3 +14,27 @@ for d in data:
     sum_len += len(d)
     
 print('留言的平均长度是',sum_len/len(data))
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有',len(new),'笔留言长度小于100')
+print(new[0])
+print(new[1])
+
+# good = []
+# for d in data:
+#     if 'good' in d:
+#         good.append(d)
+print('一共有', len(good), '笔留言提到good')
+
+good = [d for d in data if 'good' in d]
+print(good)
+
+bad = ['bad' in d for d in data]
+print('bad')
+
+bad = []
+for d in data:
+    bad.append('bad' in d)
